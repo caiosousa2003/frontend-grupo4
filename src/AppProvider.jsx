56bdb69a8943924d276b9175b0d799fd9ed3bd4a
Otavio <otavioaugustobr@gmail.com>
2024-05-13 18:24:26 -0300

    Correção de código
    Correção de Home

--- src/AppProvider.jsx
+++ src/AppProvider.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import Home from './pages/Home/Home';
+//import Cadastro from './pages/Cadastro/Cadastro';
 
 function App() {
   return (
-    <>
-      <div>
-        <Home />
-      </div>
-    </>
+    <div>
+      <Home />
+    </div>
   );
 }
 
