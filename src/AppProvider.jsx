2c49935c098fcaf76e0d960b9c3d3d0d6ae89809
matheusmadeira <fumacammadeira@gmail.com>
2024-05-21 20:50:41 -0300

    Mais outra extensão

--- src/AppProvider.jsx
+++ src/AppProvider.jsx
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
 import Routes from "./routes";
 import GlobalStyles from "./styles/globalSytles";
 
 function App() {
   return (
     <div>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Routes></Routes>
     </div>
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Routes from "./routes";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <Routes></Routes>
-    </>
->>>>>>> PaginaCadastro
   );
 }
-
 export default App;
