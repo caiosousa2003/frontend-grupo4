57d4e0e47c8e699e4e24788f2465d31d3d01638c
caiosousa2003 <caiosousa080403@gmail.com>
2024-05-30 22:16:41 -0300

    Preparação

--- src/pages/Cadastro/styleCadastro.js
+++ src/pages/Cadastro/styleCadastro.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { colors } from "../../styles/sytlesVariables";
 
 export const DivPrincipal = styled.div`
   display: flex;
@@ -16,3 +17,14 @@ export const TituloCadastro = styled.h1`
   color: yellow;
   font-size: 48px;
 `;
+
+export const Alert = styled.p`
+  color: ${colors.font.secondary};
+  text-transform: uppercase;
+  text-decoration: solid;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 2px;
+  text-align: left;
+  width: 43%;
+`
