367ecf8bcca16cfc620a5dccdaceae0bebb0f58d
caiosousa2003 <caiosousa080403@gmail.com>
2024-06-05 18:03:23 -0300

    Finalização do Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,30 +1,47 @@
 /* eslint-disable react/prop-types */
-import { LogoCPE, ContainerHeader, ContainerIcons, IconUsuarios, IconProjetos, ContainerAdmin, ContainerUsuarios, IconEditar, IconSair } from './styleHeader';
+import { LogoCPE, ContainerHeader, ContainerIcons, IconUsuarios, IconProjetos, ContainerAdmin, ContainerUsuarios, IconEditar, IconSair, Linha } from './styleHeader';
 import logo from '../../assets/LogoCadastro.png';
 import { IoLogOutOutline } from "react-icons/io5";
-import iconEditar from '../../assets/IconEditPreto.png';
+import { FaRegEdit } from "react-icons/fa";
 import iconUsuarios from '../../assets/IconUsuarios.png';
 import iconProjetos from '../../assets/IconProjetos.png';
+import { useLocation, useNavigate } from 'react-router-dom';
+import useAuthStore from '../../Stores/auth';
 
 function Header() {
 
+  const navigate = useNavigate();
 
+  let home;
+  if(useLocation().pathname == "/"){
+    home = true;
+  }else{
+    home = false;
+  }
 
-
+  let nivel;
+  if(useAuthStore.getState().usuario){
+    nivel = useAuthStore.getState().usuario.usuario.nivel;
+  }else{
+    nivel = false;
+  }
 
   return (
     <div>
       <ContainerHeader>
-        <LogoCPE src={logo} alt="logo"></LogoCPE>
+        <LogoCPE src={logo} alt="logo" onClick={() => navigate('/')}></LogoCPE>
         <ContainerIcons>
-          <ContainerAdmin>
-            <IconProjetos src={iconProjetos}></IconProjetos>
-            <IconUsuarios src={iconUsuarios}></IconUsuarios>
-          </ContainerAdmin>
-          <ContainerUsuarios>
-            <IconEditar></IconEditar>
-            <IconSair><IoLogOutOutline/></IconSair>
-          </ContainerUsuarios>
+          {home && nivel &&
+            <ContainerAdmin>
+            <IconProjetos src={iconProjetos} onClick={() => navigate('/gerenciar-projetos')}></IconProjetos>
+            <IconUsuarios src={iconUsuarios} onClick={() => navigate('/gerenciar-usuarios')}></IconUsuarios>
+          </ContainerAdmin>}
+          {home &&
+            <ContainerUsuarios>
+            <IconEditar onClick={() => alert("...EDITAR PERFIL")}><FaRegEdit style={{ height: "100%", width: "100%" }}/></IconEditar>
+            <Linha></Linha>
+            <IconSair><IoLogOutOutline style={{ height: "100%", width: "100%" }}/></IconSair>
+          </ContainerUsuarios>}
         </ContainerIcons>
       </ContainerHeader>
     </div>
--- src/components/header/styleHeader.js
+++ src/components/header/styleHeader.js
@@ -11,6 +11,12 @@ export const ContainerHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media screen and (max-width: 564px) {
+    height: 12vh;
+  }
+  @media screen and (max-width: 480px) {
+    height: 10vh;
+  }
   @media screen and (max-width: 400px) {
     height: 8vh;
   }
@@ -25,35 +31,79 @@ export const ContainerIcons = styled.div`
 
 export const IconUsuarios = styled.img`
   margin-left: auto;
+  height: 95%;
+  margin-left: 15px;
   cursor: pointer;
+  @media screen and (max-width: 400px) {
+    margin-left: 7px;
+  }
 `;
 
 export const IconProjetos = styled.img`
   margin-left: auto;
+  height: 95%;
   cursor: pointer;
 `;
 
 export const ContainerAdmin = styled.div`
-
+  height: 100%;
 `;
 
 export const IconSair = styled.div`
-  margin-left: auto;
+  display: flex;
   height: 90%;
-  border-left: 3px solid black;
+  padding: 10px 7px 10px 12px;
   cursor: pointer;
+  @media screen and (max-width: 480px) {
+    padding: 7px 4px 7px 9px;
+  }
+  @media screen and (max-width: 400px) {
+    padding: 5px 2px 5px 7px;
+  }
 `;
 
 export const IconEditar = styled.div`
-  margin-left: auto;
-  border-right: 2px;
+  display: flex;
+  height: 80%;
+  padding: 10px 15px 10px 15px;
   cursor: pointer;
+  @media screen and (max-width: 480px) {
+    padding: 7px 12px 7px 12px;
+  }
+  @media screen and (max-width: 400px) {
+    padding: 5px 10px 5px 10px;
+  }
 `;
 
 export const ContainerUsuarios = styled.div`
+  display: flex;
   border: 3px solid black;
   height: 80%;
   border-radius: 10px;
   align-items: center;
-  justify-content: space-between;
+  margin-left: 30px;
+  margin-right: 20px;
+  flex-direction: row;
+  @media screen and (max-width: 480px) {
+    border: 2px solid black;
+    margin-left: 20px;
+    margin-right: 15px;
+  }
+  @media screen and (max-width: 400px) {
+    margin-left: 8px;
+    margin-right: 5px;
+  }
+`;
+
+export const Linha = styled.div`
+  display: flex;
+  top: 0;
+  left: 50%;
+  height: 100%;
+  width: 3px;
+  background-color: black;
+  transform: translateX(-50%);
+  @media screen and (max-width: 480px) {
+    width: 2px;
+  }
 `;
