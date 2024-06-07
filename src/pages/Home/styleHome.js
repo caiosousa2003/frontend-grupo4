418b7e25b0ea2129bf1e174d86d1a579c0464c68
caiosousa2003 <caiosousa080403@gmail.com>
2024-06-06 23:59:47 -0300

    Tirando comentários

--- src/pages/Home/styleHome.js
+++ src/pages/Home/styleHome.js
@@ -3,47 +3,7 @@ export const DivContainer = styled.div`
   background-color: #111;
   height: 100vh;
 `;
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  background-color: #111;
-`;
-export const Line = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 20px;
-`;
+
 export const DivModal = styled.div`
   background-color: #111;
 `;
-export const DivForm = styled.div`
-  background-color: #111;
-  color: white;
-`;
-export const TituloModal = styled.h1`
-  color: white;
-`;
-export const BodyModal = styled.div`
-  background-color: #141414;
-  display: flex;
-  color: white;
-`;
-
-export const DivModalConfirm = styled.div`
-  background-color: #111;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const FooterModal = styled.div``;
-export const Cargo = styled.div``;
-export const Nome = styled.div``;
-export const Email = styled.div``;
